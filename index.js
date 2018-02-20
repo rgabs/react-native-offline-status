c6848c6d93e59d491d20e87186f2d95b6a519c46
Johann Baziret <Johann.Baziret@gmail.com>
2018-02-20 13:39:52 +0700

    Remove PropTypes from React to PropTypes from "prop-types" because it was crashing on 0.53

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-import React, {Component, PropTypes} from 'react';
-import {NetInfo, View, StatusBar, Animated, Easing, AppState} from 'react-native';
-import styles from './index.styles';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  NetInfo,
+  View,
+  StatusBar,
+  Animated,
+  Easing,
+  AppState
+} from "react-native";
+import styles from "./index.styles";
 
 export default class OfflineBar extends Component {
   static propTypes = {
     offlineText: PropTypes.string
-  }
+  };
 
   animationConstants = {
     DURATION: 800,
     TO_VALUE: 4,
-    INPUT_RANGE: [0, .5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+    INPUT_RANGE: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
     OUTPUT_RANGE: [0, -15, 0, 15, 0, -15, 0, 15, 0]
-  }
+  };
 
-  setNetworkStatus = (status) => {
-    this.setState({isConnected: status});
+  setNetworkStatus = status => {
+    this.setState({ isConnected: status });
     if (status) {
       this.triggerAnimation();
     }
-  }
+  };
 
   state = {
     isConnected: true
-  }
-  _handleAppStateChange = (nextAppState) => {
-    if (nextAppState === 'active') {
+  };
+  _handleAppStateChange = nextAppState => {
+    if (nextAppState === "active") {
       NetInfo.isConnected.fetch().then(this.setNetworkStatus);
     }
-  }
-  componentWillMount () {
-    NetInfo.isConnected.addEventListener('change', this.setNetworkStatus);
-    AppState.addEventListener('change', this._handleAppStateChange);
+  };
+  componentWillMount() {
+    NetInfo.isConnected.addEventListener("change", this.setNetworkStatus);
+    AppState.addEventListener("change", this._handleAppStateChange);
     this.animation = new Animated.Value(0);
   }
-  componentWillUnMount () {
-    NetInfo.isConnected.removeEventListener('change', this.setNetworkStatus);
-    AppState.removeEventListener('change', this._handleAppStateChange);
+  componentWillUnMount() {
+    NetInfo.isConnected.removeEventListener("change", this.setNetworkStatus);
+    AppState.removeEventListener("change", this._handleAppStateChange);
   }
   // Took Reference from https://egghead.io/lessons/react-create-a-button-shake-animation-in-react-native#/tab-code
   triggerAnimation = () => {
@@ -47,21 +55,24 @@ export default class OfflineBar extends Component {
       useNativeDriver: true,
       ease: Easing.bounce
     }).start();
-  }
+  };
 
-  render () {
+  render() {
     const interpolated = this.animation.interpolate({
       inputRange: this.animationConstants.INPUT_RANGE,
       outputRange: this.animationConstants.OUTPUT_RANGE
     });
     const animationStyle = {
-      transform: [{translateX: interpolated}]
+      transform: [{ translateX: interpolated }]
     };
-    const {offlineText = 'You are not connected to Internet'} = this.props;
-    return !this.state.isConnected ?
+    const { offlineText = "You are not connected to Internet" } = this.props;
+    return !this.state.isConnected ? (
       <View style={[styles.container]}>
-        <StatusBar backgroundColor='#424242' />
-        <Animated.Text style={[styles.offlineText, animationStyle]}>{offlineText}</Animated.Text>
-      </View> : null;
+        <StatusBar backgroundColor="#424242" />
+        <Animated.Text style={[styles.offlineText, animationStyle]}>
+          {offlineText}
+        </Animated.Text>
+      </View>
+    ) : null;
   }
 }
